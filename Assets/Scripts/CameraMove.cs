ecea39d7a41bce8a4e0e89d55d8c4f3c8f84e897
Sidney Nimako <snibo13@gmail.com>
2022-12-30 03:43:25 -0600

    Some stuff

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,27 +5,26 @@ using Cinemachine;
 
 public class CameraMove : MonoBehaviour
 {
-    
     public GameObject spielberg;
+    public GameObject character;
     public float OffsetX;
     public float OffsetY;
-    void Start()
-    {
-        
-    }
+
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag != "Player") return;
+        if (collision.tag != "Player")
+            return;
         // spielberg.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenY = OffsetY;
         // spielberg.GetCinemachineComponent<CinemachineFramingTransposer>().m_ScreenX = OffsetX;
-        spielberg.transform.position = new Vector3(OffsetX, OffsetY, 0);
+        Debug.Log(spielberg.transform.position);
+        spielberg.transform.position =
+            character.transform.position + new Vector3(OffsetX, OffsetY, 0);
+        Debug.Log(spielberg.transform.position);
         Debug.Log("Changing Camera Shot");
     }
 }
